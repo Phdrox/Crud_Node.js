570dae5c29d263a901323faf8a7995c285d3db9b
Pedro Ximenes <phximenes2103@gmail.com>
2024-02-06 23:47:31 -0300

    crud

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const app=express();
 const router=require('./routes');
+const port=process.env.PORT || 3000
 
 
 app.use(express.json())
 app.use(router);
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Rodando');
 })
